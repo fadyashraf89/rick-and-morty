8857342efcc8c89b48228153729e8807dac6e729
fadyashraf89 <fadyashraf89@gmail.com>
2024-11-10 13:45:46 +0200

    Project Setup

---
+++ lib/Constants/colors.dart
@@ -0,0 +1,7 @@
+import "package:flutter/material.dart";
+class MyColors{
+  static const Color YellowColor = Color(0xffFFC107);
+  static const Color GreyColor = Color(0xff343A40);
+  static const Color WhiteColor = Color(0xffE1E8EB);
+
+}
